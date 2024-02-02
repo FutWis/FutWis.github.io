270cf0de1dae264440f2441ceda5105e7e860b85
FutWis <drmizayt@gmail.com>
2024-02-02 10:06:20 +0100

    Update packopener.js

--- packopener.js
+++ packopener.js
@@ -1,7 +1,8 @@
 document.getElementById("openPackButton").addEventListener("click", function () {
     // Simulate pack opening (replace with your logic)
     const packContents = [
-        { name: "Pelé", image: "players/peléicon.png", rating: 95 },
+	        { name: "Kylian Mbappé", image: "players/mbappetoty.png", rating: 96 },
+                { name: "Pelé", image: "players/peléicon.png", rating: 95 },
 		{ name: "Ronaldo", image: "players/ronaldoicon.png", rating: 94 },
 		{ name: "Zinedine Zidane", image: "players/zidaneicon.png", rating: 94 },
 		{ name: "Johan Cruyff", image: "players/cruyfficon.png", rating: 93 },
